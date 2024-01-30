9047bd17834ce9af1f2f648f2d83758d716b432d
Luiz Gustavo <lg.lopesmelo@gmail.com>
2024-01-29 21:41:41 -0300

    Initial commit

---
+++ 7DaysOfCode-SpaceCoding/Program.cs
@@ -0,0 +1,23 @@
+﻿using _7DaysOfCode_SpaceCoding;
+using RestSharp;
+using System.Text.Json;
+
+var pokeBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+var ratattaEndpoint = "rattata";
+
+var client = new RestClient(pokeBaseUrl + ratattaEndpoint);
+
+var request = new RestRequest("", Method.Get);
+
+var response = client.Execute(request);
+
+var mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
+
+//dsaoidasd
+
+Console.WriteLine(mascote.name);
+Console.WriteLine(mascote.height);
+Console.WriteLine(mascote.weight);
+foreach (var item in mascote.abilities)
+    Console.WriteLine(item.ability.name);
